826b7281f078793a18a4d6eb4ce2201f5b898c99
HMonique <161358207+HMonique@users.noreply.github.com>
2024-07-01 11:20:48 -0400

    TwoIntegerVariables

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+            int x = 10;
+            int y = 25;
+            int total = x + y;
+        System.out.println(total);
+    }
+}
