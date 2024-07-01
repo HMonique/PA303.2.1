3c534862e7b895441121642d35816e9c70628ea0
HMonique <161358207+HMonique@users.noreply.github.com>
2024-07-01 14:06:25 -0400

    Commit 1

---
+++ src/IntegerDouble.java
@@ -0,0 +1,12 @@
+public class IntegerDouble {
+    public static void main(String[] args) {
+
+        int a = 25;
+        double b = 35.5;
+        double sum =  a + b;
+        System.out.println(sum);
+
+
+
+    }
+}
