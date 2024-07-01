e5139a158391de51be7a16fa6dfd656098c0815b
HMonique <161358207+HMonique@users.noreply.github.com>
2024-07-01 14:21:51 -0400

    Commit 2

---
+++ src/TwoIntegerVariables.java
@@ -0,0 +1,15 @@
+//Write a program that declares two integer variables, assigns an integer to each, and divides the larger number by the smaller number.
+// Assign the result to a variable. Print out the result. Now change the larger number to a decimal.
+// What happens? What corrections are needed?
+
+public class TwoIntegerVariables {
+    public static void main(String[] args) {
+        int x = 5;
+//        int y = 20;
+        double y = 15.5;
+        double sum = y / x;
+        System.out.println(sum);
+    }
+}
+
+
