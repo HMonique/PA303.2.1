afb75faf22e23362d290543cae749206225cf20f
HMonique <161358207+HMonique@users.noreply.github.com>
2024-07-01 14:58:27 -0400

    Commit 1

---
+++ src/DoubleVariable.java
@@ -0,0 +1,8 @@
+public class DoubleVariable {
+    public static void main(String[] args) {
+        double a = 22;
+        double b = 14;
+        double sum = a / b;
+        System.out.println(sum);
+    }
+}
