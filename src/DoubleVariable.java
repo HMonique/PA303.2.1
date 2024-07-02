8331ca8167575fdc7a096a9ce1af9474c8bdbaec
HMonique <161358207+HMonique@users.noreply.github.com>
2024-07-02 12:15:03 -0400

    Commit 2

--- src/DoubleVariable.java
+++ src/DoubleVariable.java
@@ -1,7 +1,7 @@
 public class DoubleVariable {
     public static void main(String[] args) {
-        double a = 22;
-        double b = 14;
+        double a = 22.0;
+        double b = 14.5;
         double sum = a / b;
         System.out.println(sum);
     }
