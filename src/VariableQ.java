d58a1ca9b670bb39dba05721c4852c988c65e202
HMonique <161358207+HMonique@users.noreply.github.com>
2024-07-02 12:24:19 -0400

    Commit 2

--- src/VariableQ.java
+++ src/VariableQ.java
@@ -1,8 +1,10 @@
 public class VariableQ {
     public static void main(String[] args) {
         int x = 5;
-        int y = 6;
-        int q = y/x;
+//        int y = 6;
+        double y = 6.5;
+//        int q = y/x;
+        double q = y/x;
         System.out.println(q);
     }
 }
