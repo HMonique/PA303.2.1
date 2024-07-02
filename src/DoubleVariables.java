aa2ebad9ee37cfe7c36fd46d8f781035118ed539
HMonique <161358207+HMonique@users.noreply.github.com>
2024-07-02 15:34:05 -0400

    Commit 3

--- src/DoubleVariables.java
+++
@@ -1,8 +0,0 @@
-public class DoubleVariables {
-    public static void main(String[] args) {
-        double x = 20.5;
-        double y = 30.8;
-        double sum = x + y;
-        System.out.println(sum);
-    }
-}
---
+++ src/com/perscholas/java_basics/DoubleVariables.java
@@ -0,0 +1,16 @@
+public class DoubleVariables {
+    public static void main(String[] args) {
+        double x = 20.5;
+        double y = 30.8;
+        double sum = y/x;
+        System.out.println(sum);
+        int z = (int) (sum);
+
+        System.out.println(z);
+
+    }
+}
+//Write a program that declares two double variables, assigns a
+// number to each, and divides the larger by the smaller number.
+// Assign the result to a variable. Print out the result. Now,
+// cast the result to an integer. Print out the result again.
