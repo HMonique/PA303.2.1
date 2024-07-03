0b54191c4e51e94971dd4427f20218fc85f52249
HMonique <161358207+HMonique@users.noreply.github.com>
2024-07-03 11:03:20 -0400

    Commit 4

---
+++ src/Constant.java
@@ -0,0 +1,18 @@
+public class Constant {
+    public static void main(String[] args) {
+    double latte = 3.00;
+    double mocha = 4.50;
+    double chai = 2.50;
+
+    double subtotal = 0.0;
+    double totalSale = 0.0;
+
+    subtotal = (3 * latte) + (4 * mocha) + (2 * chai);
+
+    final double SALES_TAX = 8.875;
+    totalSale = subtotal + (subtotal * SALES_TAX);
+
+        System.out.println("Subtotal:" +subtotal);
+
+    }
+}
